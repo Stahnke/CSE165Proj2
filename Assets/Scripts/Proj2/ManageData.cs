1b097e828a14d71dfcae2dd800bea2b543788765
Ty Stahnke <pstahnke@ucsd.edu>
2017-02-08 00:53:18 -0800

    Finished disk r/w

--- Assets/Scripts/Proj2/ManageData.cs
+++ Assets/Scripts/Proj2/ManageData.cs
@@ -5,32 +5,46 @@ using System;
 
 public class ManageData : MonoBehaviour {
 
-    ArrayList objects;
     public GameObject[] prefabs;
+    private ArrayList objects  = new ArrayList();
+    private ArrayList allLines = new ArrayList();
 
     void Start()
     {
-        objects = new ArrayList();
+        //TESTING
+        LoadData("myTest.txt");
+        SaveData("myTest.txt");
+        LoadData("myTest.txt");
+        SaveData("myTest.txt");
+        LoadData("myTest.txt");
+        SaveData("myTest.txt");
+        LoadData("myTest.txt");
+        SaveData("myTest.txt");
+        LoadData("myTest.txt");
+        SaveData("myTest.txt");
         LoadData("myTest.txt");
         SaveData("myTest.txt");
     }
 
     void SaveData(string filename)
     {
+        //Empty the lines to write to the file
+        allLines.Clear();
+
+        //Begin saving data
         print("Saving Data");
 
         //Set up path
         string path = Application.dataPath + "/Resources/Files/" + filename;
 
-        ArrayList allLines = new ArrayList();
         float xpos, ypos, zpos;
         float xrot, yrot, zrot;
         GameObject curObj;
 
-        //
-        for(int x = 0; x < objects.Count; x++)
+        //For each object in our array, get the data and write it
+        for(int i = 0; i < objects.Count; i++)
         {
-            curObj = (GameObject)objects.ToArray()[x];
+            curObj = (GameObject)objects.ToArray()[i];
 
             //Get pos
             xpos = curObj.transform.position.x;
@@ -39,26 +53,36 @@ public class ManageData : MonoBehaviour {
 
             //***FIX THIS***//
             //Get rot
-            xrot = curObj.transform.rotation.x;
-            yrot = curObj.transform.rotation.y;
-            zrot = curObj.transform.rotation.z;
-
+            xrot = curObj.transform.eulerAngles.x;
+            yrot = curObj.transform.eulerAngles.y;
+            zrot = curObj.transform.eulerAngles.z;
 
             print(curObj.name);
             print(xpos + " " + ypos + " " + zpos);
             print(xrot + " " + yrot + " " + zrot);
 
-            string line = "6" + "," + xpos + "," + ypos + "," + zpos + "," + xrot + "," + yrot + "," + zrot;
+            string line = curObj.name + "," + xpos + "," + ypos + "," + zpos + "," + xrot + "," + yrot + "," + zrot;
             allLines.Add(line);
         }
 
+        //Write all lines to file
         System.IO.File.WriteAllLines(path, (string[])allLines.ToArray(typeof(string)));
     }
 
     void LoadData(string filename)
     {
-        print("Loading Data");
+        //Delete all the objects currently on the scene
+        print("Deleting scene");
+
+        while(objects.Count > 0)
+        {
+            GameObject.Destroy((GameObject)objects[0]);
+            objects.RemoveAt(0);
+        }
         
+        //Begin loading process
+        print("Loading Data");
+
         //Set up path
         string path = Application.dataPath + "/Resources/Files/" + filename;
 
@@ -74,28 +98,32 @@ public class ManageData : MonoBehaviour {
         //Read file
         while((line = file.ReadLine()) != null)
         {
-            //READ FROM FILE
-            List<float> numbers = new List<float>(Array.ConvertAll(line.Split(','), float.Parse));
+            if(line != "\n" && line != "")
+            {
+                //READ FROM FILE
+                List<float> numbers = new List<float>(Array.ConvertAll(line.Split(','), float.Parse));
 
-            //Set all values
-            objectNum = (int)numbers[0];
-            xpos = numbers[1];
-            ypos = numbers[2];
-            zpos = numbers[3];
-            xrot = numbers[4];
-            yrot = numbers[5];
-            zrot = numbers[6];
-            curPrefab = prefabs[objectNum];
+                //Set all values
+                objectNum = (int)numbers[0];
+                xpos = numbers[1];
+                ypos = numbers[2];
+                zpos = numbers[3];
+                xrot = numbers[4];
+                yrot = numbers[5];
+                zrot = numbers[6];
+                curPrefab = prefabs[objectNum];
 
-            //Load into Vector3 and Quaternion
-            Vector3 position = new Vector3(xpos, ypos, zpos);
-            Quaternion rotation = Quaternion.Euler(xrot, yrot, zrot);
+                //Load into Vector3 and Quaternion
+                Vector3 position = new Vector3(xpos, ypos, zpos);
+                Vector3 rotation = new Vector3(xrot, yrot, zrot);
 
-            //Instantiate
-            GameObject curObj = Instantiate(curPrefab, position, rotation) as GameObject;
-            objects.Add(curObj);
+                //Instantiate
+                GameObject curObj = Instantiate(curPrefab, position, Quaternion.identity) as GameObject;
+                curObj.transform.eulerAngles = rotation;
+                curObj.name = objectNum + "";
+                objects.Add(curObj);
+            }
         }
-
         file.Close();
     }
 }
